9cc0f93c35475748f92e95ac60bb257f6f6e7090
pabloreda <pabloreda@e1b2fb66-2cfc-11de-a28d-194e1977add9>
2010-04-12 15:35:32 +0000

    r4java aproach

--- r4java/r4java.java
+++ r4java/r4java.java
@@ -17,6 +17,212 @@ import java.util.Calendar;
 
 public class r4java extends tinyptc implements KeyListener, MouseListener, MouseMotionListener {
 
+enum {
+FIN,LIT,ADR,CALL,JMP,JMPR, EXEC,//hasta JMPR no es visible
+IF,PIF,NIF,UIF,IFN,IFL,IFG,IFLE,IFGE,IFNO,IFAND,IFNAND,// condicionales 0 - y +  y no 0
+DUP,DROP,OVER,PICK2,PICK3,PICK4,SWAP,NIP,ROT,
+DUP2,DROP2,DROP3,DROP4,OVER2,SWAP2,//--- pila
+TOR,RFROM,ERRE,ERREM,ERRFM,ERRSM,ERRDR,//--- pila direcciones
+AND,OR,XOR,NOT,//--- logica
+SUMA,RESTA,MUL,DIV,MULDIV,MULSHR,DIVMOD,MOD,ABS,
+NEG,INC,INC4,DEC,DIV2,MUL2,SHL,SHR,//--- aritmetica
+FECH,CFECH,WFECH,STOR,CSTOR,WSTOR,INCSTOR,CINCSTOR,WINCSTOR,//--- memoria
+FECHPLUS,STOREPLUS,CFECHPLUS,CSTOREPLUS,WFECHPLUS,WSTOREPLUS,
+MOVED,MOVEA,CMOVED,CMOVEA,
+MEM,PATH,BFILE,BFSIZE,VOL,LOAD,SAVE,//--- bloques de memoria, bloques
+UPDATE,
+TPEN,
+XYMOUSE,BMOUSE,
+IRKEY,KEY,
+CNTJOY,GETJOY,
+MSEC,TIME,IDATE,SISEND,SISRUN,//--- sistema
+WIDTH,HEIGHT,CLS,REDRAW,FRAMEV,//--- pantalla
+SETXY,MPX,SPX,GPX,
+VXFB,TOXFB,XFBTO,
+COLORF,COLOR,COLORA,ALPHA,//--- color
+OP,CP,LINE,CURVE,PLINE,PCURVE,POLI,//--- dibujo
+FCOL,FCEN,FMAT,SFILL,LFILL,RFILL,TFILL, //--- pintado
+SLOAD,SPLAY,MLOAD,MPLAY,
+SERVER,CLIENT,NSEND,RECV,CLOSE, //---- red
+DOCINI,DOCEND,DOCMOVE,DOCLINE,DOCTEXT,DOCFONT,DOCBIT,DOCRES,DOCSIZE,
+SYSTEM,
+ULTIMAPRIMITIVA// de aqui en mas.. apila los numeros 0..255-ULTIMAPRIMITIVA
+};
+
+	int ip;
+	int [] dpila = new int[1024];
+	int [] rpila = new int[1024];
+	int sizefb = width * height;
+	int FB[] = new int[sizefb];
+	int XFB[] = new int[sizefb];
+
+	char memcod[] = new char[256*1024];
+	char memdat[] = new char[1024*1024];
+
+	public void interprete()
+	{
+	int TOS,NOS,RTOS;
+	char op;
+	TOS=NOS=RTOS=0;
+	while (true) {
+		op=memcod[ip];
+		ip=ip+1;
+		switch(op) {
+			case FIN: ip=rpila[RTOS];RTOS--;break
+			case LIT: NOS++;dpila[NOS]=TOS;TOS=memcod[ip]|memcod[ip+1]<<8|memcod[ip+2]<<16|memcod[ip+3]<<24;ip=ip+4;break;
+			case ADR: NOS++;dpila[NOS]=TOS;TOS=memcod[ip]|memcod[ip+1]<<8|memcod[ip+2]<<16|memcod[ip+3]<<24;ip=ip+4;TOS=... break;
+			case CALL:
+			case JMP:
+			case JMPR:
+			case EXEC://hasta JMPR no es visible
+			case IF:	// condicionales 0 - y +  y no 0
+			case PIF:
+			case NIF:
+			case UIF:
+			case IFN:
+			case IFL:
+			case IFG:
+			case IFLE:
+			case IFGE:
+			case IFNO:
+			case IFAND:
+			case IFNAND:
+			case DUP: NOS++;dpila[NOS]=TOS;break;//--- pila
+			case DROP:
+			case OVER:
+			case PICK2:
+			case PICK3:
+			case PICK4:
+			case SWAP:
+			case NIP:
+			case ROT:
+			case DUP2:
+			case DROP2:
+			case DROP3:
+			case DROP4:
+			case OVER2:
+			case SWAP2:
+			case TOR:	break;//--- pila direcciones
+			case RFROM:
+			case ERRE:
+			case ERREM:
+			case ERRFM:
+			case ERRSM:
+			case ERRDR:
+			case AND:	TOS=dpila[NOS]&TOS;NOS--;break; //--- aritmetica
+			case OR:	TOS=dpila[NOS]|TOS;NOS--;break;
+			case XOR:	TOS=dpila[NOS]^TOS;NOS--;break;
+			case NOT:	TOS=not TOS;break;
+			case SUMA:	TOS=dpila[NOS]+TOS;NOS--;break;
+			case RESTA:	TOS=dpila[NOS]-TOS;NOS--;break;
+			case MUL:	TOS=dpila[NOS]*TOS;NOS--;break;
+			case DIV:	TOS=dpila[NOS]/TOS;NOS--;break;
+			case MULDIV:
+			case MULSHR:
+			case DIVMOD:
+			case MOD:
+			case ABS:
+			case NEG:
+			case INC:
+			case INC4:
+			case DEC:
+			case DIV2:	TOS=TOS/2;break;
+			case MUL2:	TOS=TOS*2;break;
+			case SHL:
+			case SHR:
+			case FECH:break;//--- memoria
+			case CFECH:
+			case WFECH:
+			case STOR:
+			case CSTOR:
+			case WSTOR:
+			case INCSTOR:
+			case CINCSTOR:
+			case WINCSTOR:
+			case FECHPLUS:
+			case STOREPLUS:
+			case CFECHPLUS:
+			case CSTOREPLUS:
+			case WFECHPLUS:
+			case WSTOREPLUS:
+			case MOVED://--- bloques de memoria: bloques
+			case MOVEA:
+			case CMOVED:
+			case CMOVEA:
+			case MEM:
+			case PATH:
+			case BFILE:
+			case BFSIZE:
+			case VOL:
+			case LOAD:
+			case SAVE:
+			case UPDATE://--- sistema
+			case TPEN:
+			case XYMOUSE:
+			case BMOUSE:
+			case IRKEY:
+			case KEY:
+			case CNTJOY:
+			case GETJOY:
+			case MSEC:
+			case TIME:
+			case IDATE:
+			case SISEND:
+			case SISRUN:
+			case WIDTH:
+			case HEIGHT:
+			case CLS:
+			case REDRAW: update(FB);break;
+			case FRAMEV://--- pantalla
+			case SETXY:
+			case MPX:
+			case SPX:
+			case GPX:
+			case VXFB:
+			case TOXFB:
+			case XFBTO:
+			case COLORF:
+			case COLOR:
+			case COLORA:
+			case ALPHA://--- color
+			case OP:
+			case CP:
+			case LINE:
+			case CURVE:
+			case PLINE:
+			case PCURVE:
+			case POLI://--- dibujo
+			case FCOL:
+			case FCEN:
+			case FMAT:
+			case SFILL:
+			case LFILL:
+			case RFILL:
+			case TFILL: //--- pintado
+			case SLOAD:
+			case SPLAY:
+			case MLOAD:
+			case MPLAY:
+			case SERVER:
+			case CLIENT:
+			case NSEND:
+			case RECV:
+			case CLOSE: //---- red
+			case DOCINI:
+			case DOCEND:
+			case DOCMOVE:
+			case DOCLINE:
+			case DOCTEXT:
+			case DOCFONT:
+			case DOCBIT:
+			case DOCRES:
+			case DOCSIZE:
+			case SYSTEM:
+
+			  }
+		}
+	}
+
     public void main(int width,int height) {
 
         int size = width * height;
@@ -65,7 +271,7 @@ public class r4java extends tinyptc implements KeyListener, MouseListener, Mouse
     public void keyPressed(KeyEvent e) {
         displayInfo(e, "KEY PRESSED: ");
     }
-    
+
     public void keyReleased(KeyEvent e) {
         displayInfo(e, "KEY RELEASED: ");
     }
