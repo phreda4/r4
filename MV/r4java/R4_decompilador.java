96ff843fc8d7475d4cb5112446ff260596834ff4
manuel.cornes@gmail.com <manuel.cornes@gmail.com@e1b2fb66-2cfc-11de-a28d-194e1977add9>
2011-03-13 11:34:33 +0000

    New version of the java r4 bytecode decompiler. Example program and output.

--- MV/r4java/R4_decompilador.java
+++ MV/r4java/R4_decompilador.java
@@ -10,36 +10,30 @@
 //  Reda4 interprete y compilador  pabloreda@gmail.com
 //  Version Java Applet
 //
-// MC 26/3/2010 De-compilador v1, this program de-compiles the bytecode file passed on the command line. This is not an applet.
+// MC 26/03/2010 De-compilador v1, this program de-compiles the bytecode file passed on the command line. This is not an applet.
+// MC 13/03/2011 De-compilador v2, adapted to new bytecodes, changed output format
 
 import java.io.*;
 
 class R4_decompilador {
 
-    public static final int FIN= 0,LIT= 1,ADR= 2,CALL= 3,JMP=4 ,JMPR= 5,EXEC=6 ,
-	IF=7 ,PIF= 8,NIF= 9,UIF=10 ,IFN= 11,IFL= 12,IFG=13 ,IFLE= 14,IFGE= 15,IFNO=16 ,IFAND=17 ,IFNAND=18 ,
-	DUP= 19,DROP=20 ,OVER= 21,PICK2=22 ,PICK3= 23,PICK4= 24,SWAP= 25,NIP= 26,ROT= 27,
-	DUP2= 28,DROP2=29 ,DROP3=30 ,DROP4=31 ,OVER2=32 ,SWAP2=33 ,
-	TOR=34 ,RFROM=35 ,ERRE=36 ,ERREM=37 ,ERRFM=38 ,ERRSM=39 ,ERRDR=40 ,
-	AND=41 ,OR= 42,XOR=43 ,NOT=44 ,
-	SUMA=45 ,RESTA=46 ,MUL= 47,DIV=48 ,MULDIV=49 ,MULSHR= 50,DIVMOD=51 ,MOD=52 ,ABS=53 ,
-	NEG= 54,INC=55 ,INC4=56 ,DEC=57 ,DIV2=58 ,MUL2=59 ,SHL=60 ,SHR=61 ,
-	FECH= 62,CFECH=63 ,WFECH=64 ,STOR=65 ,CSTOR=66 ,WSTOR=67 ,INCSTOR=68 ,CINCSTOR=69 ,WINCSTOR=70 ,
-	FECHPLUS=71 ,STOREPLUS=72 ,CFECHPLUS= 73,CSTOREPLUS=74 ,WFECHPLUS=75 ,WSTOREPLUS= 76,
-	MOVED=77 ,MOVEA=78 ,CMOVED= 79,CMOVEA=80 ,
-	MEM=81 ,PATH=82 ,BFILE=83 ,BFSIZE= 84,VOL= 85,LOAD= 86,SAVE=87 ,
-	UPDATE=88 ,
-	TPEN=89 ,
-	XYMOUSE=90 ,BMOUSE=91 ,
-	IRKEY=92 ,KEY=93 ,
-	CNTJOY=94 ,GETJOY=95 ,
-	MSEC= 96,TIME=97 ,IDATE=98 ,SISEND=99 ,SISRUN=100 ,
-	WIDTH=101 ,HEIGHT=102 ,CLS=103 ,REDRAW=104 ,FRAMEV=105 ,
-	SETXY=106 ,MPX= 107,SPX=108 ,GPX= 109,
-	VXFB=110 ,TOXFB= 111,XFBTO=112 ,
-	COLORF=113 ,COLOR=114 ,COLORA=115 ,ALPHA= 116,
-	OP=117 ,CP= 118,LINE=119 ,CURVE=120 ,PLINE=121 ,PCURVE=122 ,POLI=123 ,
-	FCOL=124 ,FCEN=125 ,FMAT=126 ,SFILL=127 ,LFILL= 128,RFILL=129 ,TFILL=130 ;
+    public static final int
+	FIN= 0,LIT= 1,ADR= 2,CALL= 3,JMP=4 ,JMPR= 5,EXEC=6 , IF=7 ,PIF= 8,NIF= 9,UIF=10 ,
+	IFN= 11,IFL= 12,IFG=13 ,IFLE= 14,IFGE= 15,IFNO=16 ,IFAND=17 ,IFNAND=18 , DUP= 19,DROP=20 ,
+	OVER= 21,PICK2=22 ,PICK3= 23,PICK4= 24,SWAP= 25,NIP= 26,ROT= 27, DUP2= 28,DROP2=29 ,DROP3=30 ,
+	DROP4=31 ,OVER2=32 ,SWAP2=33 , TOR=34 ,RFROM=35 ,ERRE=36 ,ERREM=37 ,ERRFM=38 ,ERRSM=39 ,ERRDR=40 ,
+	AND=41 ,OR= 42,XOR=43 ,NOT=44 , SUMA=45 ,RESTA=46 ,MUL= 47,DIV=48 ,MULDIV=49 ,MULSHR= 50,
+	DIVMOD=51 ,MOD=52 ,ABS=53 , CSQRT=54,CLZ=55,CDIVSH=56, NEG= 57,INC=58 ,INC4=59 ,DEC=60 ,
+	DIV2=61 ,MUL2=62 ,SHL=63 ,SHR=64 , FECH= 65,CFECH=66 ,WFECH=67 ,STOR=68 ,CSTOR=69 ,WSTOR=70 ,
+	INCSTOR=71 ,CINCSTOR=72 ,WINCSTOR=73 , FECHPLUS=74 ,STOREPLUS=75 ,CFECHPLUS= 76,CSTOREPLUS=77 ,WFECHPLUS=78 ,WSTOREPLUS= 79, MOVED=80 ,
+	MOVEA=81 ,CMOVED= 82,CMOVEA=83 , MEM=84 ,PATH=85 ,BFILE=86 ,BFSIZE= 87,VOL= 88,LOAD= 89,SAVE=90 ,
+	UPDATE=91 , XYMOUSE=92 ,BMOUSE=93 , SKEY=94 , KEY=95 , CNTJOY=96 ,GETJOY=97 , MSEC= 98,TIME=99 ,IDATE=100 ,
+	SISEND=101 ,SISRUN=102 , WIDTH=103 ,HEIGHT=104 ,CLS=105 ,REDRAW=106 ,FRAMEV=107 , SETXY=108 ,MPX= 109,SPX=110 ,
+	GPX= 111, VXFB=112 ,TOXFB= 113,XFBTO=114 , COLORF=115 ,COLOR=116 ,COLORA=117 ,ALPHA= 118, OP=119 ,CP= 120,
+	LINE=121 ,CURVE=122 ,PLINE=123 ,PCURVE=124 ,POLI=125 , FCOL=126 ,FCEN=127 ,FMAT=128 ,SFILL=129 ,LFILL= 130,
+	RFILL=131 ,TFILL=132, SYSTEM=133,
+
+	ULTIMAPRIMITIVA= 148 ;
 
     public static String[] bytecode = {
 	"FIN","LIT","ADR","CALL","JMP","JMPR","EXEC",//hasta JMPR no es visible
@@ -49,16 +43,13 @@ class R4_decompilador {
 	"TOR","RFROM","ERRE","ERREM","ERRFM","ERRSM","ERRDR",//--- pila direcciones
 	"AND","OR","XOR","NOT",//--- logica
 	"SUMA","RESTA","MUL","DIV","MULDIV","MULSHR","DIVMOD","MOD","ABS",
+	"CSQRT", "CLZ", "CDIVSH", 
 	"NEG","INC","INC4","DEC","DIV2","MUL2","SHL","SHR",//--- aritmetica
 	"FECH","CFECH","WFECH","STOR","CSTOR","WSTOR","INCSTOR","CINCSTOR","WINCSTOR",//--- memoria
 	"FECHPLUS","STOREPLUS","CFECHPLUS","CSTOREPLUS","WFECHPLUS","WSTOREPLUS",
 	"MOVED","MOVEA","CMOVED","CMOVEA",
 	"MEM","PATH","BFILE","BFSIZE","VOL","LOAD","SAVE",//--- bloques de memoria, bloques
-	"UPDATE",
-	"TPEN",
-	"XYMOUSE","BMOUSE",
-	"IRKEY","KEY",
-	"CNTJOY","GETJOY",
+	"UPDATE", "XYMOUSE","BMOUSE", "SKEY","KEY", "CNTJOY","GETJOY",
 	"MSEC","TIME","IDATE","SISEND","SISRUN",//--- sistema
 	"WIDTH","HEIGHT","CLS","REDRAW","FRAMEV",//--- pantalla
 	"SETXY","MPX","SPX","GPX",
@@ -66,7 +57,6 @@ class R4_decompilador {
 	"COLORF","COLOR","COLORA","ALPHA",//--- color
 	"OP","CP","LINE","CURVE","PLINE","PCURVE","POLI",//--- dibujo
 	"FCOL","FCEN","FMAT","SFILL","LFILL","RFILL","TFILL", //--- pintado
-	"SERVER","CLIENT","NSEND","RECV","CLOSE", //---- red
 	"SYSTEM",
 	"ULTIMAPRIMITIVA"// de aqui en mas.. apila los numeros 0..255-ULTIMAPRIMITIVA
     };
@@ -113,11 +103,18 @@ class R4_decompilador {
 	}
     }
 
-    public static int getw( byte[] prog, int idx) {
+    public static int getb( byte[] prog, int idx) {
 
 	int val ;
 
-	// in Java, signed/unsigned is, errr, different
+	val = (int)((prog[idx]) & 0xFF) ;
+
+	return val ;
+    }
+
+    public static int getw( byte[] prog, int idx) {
+
+	int val ;
 
 	val = (int)((prog[idx+1]) & 0xFF) << 8;
 	val += (int)(prog[idx] & 0xFF);
@@ -125,22 +122,9 @@ class R4_decompilador {
 	return val ;
     }
 
-    public static int getb( byte[] prog, int idx) {
-
-	int val ;
-
-	// in Java, signed/unsigned is, errr, different
-
-	val = (int)((prog[idx]) & 0xFF) ;
-
-	return val ;
-    }
-
     public static int getdw( byte[] prog, int idx) {
 
 	int val ;
-
-	// in Java, signed/unsigned is, errr, different
 
 	val  = ((int)(prog[idx+3]) & 0xFF) << 24;
 	val += ((int)(prog[idx+2]) & 0xFF) << 16;
@@ -151,11 +135,11 @@ class R4_decompilador {
     }
 
     public static void codeline(int w) {
-	System.out.print( bytecode[w] );
+	System.out.print( " = " + bytecode[w] ) ;
     }
 
     public static void codeline_v(int w, int value, String format ) {
-	System.out.print( bytecode[w] + " " + String.format("$" + format, value ) ) ;
+	System.out.print( "| " + bytecode[w] + " " + String.format( format, value ) ) ;
     }
     
     public static void itpt( byte[] prog) {
@@ -163,30 +147,36 @@ class R4_decompilador {
 	int bootadr, cntdato, cntprog ;
 	int idx=0;
 
-	System.out.print( "\n\nDecompiler :\n\n") ;
+	System.out.println("\n") ;
 
 	bootadr = getdw(prog, idx);
 	idx += 4;
-	System.out.println( "Boot adress: " + String.format("$%04x ", bootadr ) ) ;
+	System.out.println( "Boot adress: " + String.format("%04x ", bootadr ) ) ;
+
+	System.out.println( "") ;
 
 	cntdato = getdw(prog, idx);
 	idx += 4;
-	System.out.println("");
-	System.out.println( "Cnt dato: " + String.format("%04x ", cntdato ) ) ;
+	System.out.println( "# of bytes of data : " + String.format("%04x ", cntdato ) ) ;
 
 	cntprog = getdw(prog, idx);
 	idx += 4;
-	System.out.println( "Cnt prog: " + String.format("%04x ", cntprog ) ) ;
+	System.out.println( "# of bytes of code : " + String.format("%04x ", cntprog ) ) ;
 
-	System.out.println("");
-	System.out.println("Start interpreter.") ;
+	System.out.println( "" ) ;
 
 	for (int i=idx; i<idx+cntprog; i++) {
 
 	    int value ;
 	    int w = prog[i] & 0xff;
+	    int ip_print = 0x20426ff0 + i -12 ;
+	    String flag ;
 
-	    System.out.print ( i + " : " + String.format("%02x = ", w) ) ;
+	    flag = "   " ;
+	    if (ip_print == bootadr)
+		flag = "-> " ;
+
+	    System.out.print ( flag + String.format("%04x", ip_print) + " : " + String.format("%02x ", w) ) ;
 
 	    switch ( w ) {
 		
@@ -194,14 +184,20 @@ class R4_decompilador {
 	    case ADR :
 	    case CALL :
 	    case JMP :
-	    case JMPR :
 
+		for(int j=1;j<5;j++) {
+		    int val = getb(prog, i+j);
+		    System.out.print ( String.format("%02x ", val) ) ;
+		}
+		
 		value = getdw(prog, i+1);
 		i += 4 ;
+		    
 		codeline_v(w, value, "%04x" ) ;
 
 		break ;
 
+	    case JMPR :
 	    case IF :
 	    case PIF :
 	    case NIF :
@@ -215,8 +211,12 @@ class R4_decompilador {
 	    case IFAND :
 	    case IFNAND :
 		
-		value = getb(prog, i+1);
+		int val = getb(prog, i+1);
+		System.out.print ( String.format("%02x ", val) ) ;
+
+		value = getb(prog, i+1) + 1 ; /* ?? */
 		i += 1 ;
+		System.out.print ( "         ") ;
 		codeline_v(w, value, "%01x" ) ;
 		
 		break ;
@@ -224,9 +224,9 @@ class R4_decompilador {
 	    default :
 
 		if ( w < bytecode.length )
-		    System.out.print ( bytecode[ w ] ) ;
+		    System.out.print ( "            | " + bytecode[ w ] ) ;
 		else
-		    System.out.print ( String.format("%01x ", w - 150 ) ) ;
+		    System.out.print ( String.format( "            | %02x ", w - ULTIMAPRIMITIVA ) ) ;
 
 		break ;
 
@@ -239,18 +239,13 @@ class R4_decompilador {
 
     public static void main ( String[] args ) {
 
-	// bytearray pour code
-	// bytearray pour data
-	
 	int i ;
 	byte[] test ;
 	File file = new File(args[0]);
 
-	System.out.println("Decompilando: " + args[0]) ;
-
 	try {
 	    test = getBytesFromFile( file );
-	    hexdump(test);
+	    //	    hexdump(test);
 	    itpt(test);
 	}
 	catch ( IOException iox ) {
