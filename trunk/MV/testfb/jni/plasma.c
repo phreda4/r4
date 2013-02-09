4b8890c6ac9655ce777418a763bde436fc4122c0
pabloreda <pabloreda@e1b2fb66-2cfc-11de-a28d-194e1977add9>
2013-02-09 05:49:02 +0000

    read asset things (for make real filesystem)

--- trunk/MV/testfb/jni/plasma.c
+++ trunk/MV/testfb/jni/plasma.c
@@ -24,6 +24,7 @@
 #include <sys/time.h>
 #include <time.h>
 #include <android/log.h>
+#include <android/asset_manager.h>
 
 #include <sys/resource.h>
 #include <sys/syscall.h>
@@ -1011,6 +1012,150 @@ return CODIGOERROR;
 unsigned char testp[]= { LIT,1,0,0,0,LIT,3,0,0,0,AND,FIN };
 
 char *rootpath="/data/app/com.rforth.test/res/raw";
+/*
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+
+@Override
+public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    CopyAssets();
+}
+
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+}
+
+
+private void CopyAssets() {
+    AssetManager assetManager = getAssets();
+    String[] files = null;
+    try {
+        files = assetManager.list("");
+    } catch (IOException e) {
+        Log.e("tag", e.getMessage());
+    }
+    for(String filename : files) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+          in = assetManager.open(filename);
+          out = new FileOutputStream("/sdcard/" + filename);
+          copyFile(in, out);
+          in.close();
+          in = null;
+          out.flush();
+          out.close();
+          out = null;
+        } catch(Exception e) {
+            Log.e("tag", e.getMessage());
+        }
+    }
+}
+private void copyFile(InputStream in, OutputStream out) throws IOException {
+    byte[] buffer = new byte[1024];
+    int read;
+    while((read = in.read(buffer)) != -1){
+      out.write(buffer, 0, read);
+    }
+}
+}
+
+<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+*/
+
+/*
+
+void installzip()
+{
+	String apkFilePath = null;
+	ApplicationInfo appInfo = null;
+	PackageManager packMgmr = mActivity.getPackageManager();
+	appInfo = packMgmr.getApplicationInfo( "com.abc.YourPackageName", 0 );
+	apkFilePath = appInfo.sourceDir;
+
+zip* m_pArchive;
+m_pArchive = zip_open(gApkLoc.c_str(), 0, NULL);
+if (m_pArchive == NULL)
+	{
+	LOGE("Error loading APK %s", gApkLoc.c_str());
+	return;
+	}
+int numFiles = zip_get_num_files(pArchive);
+//Loop through all files
+for (int i = 0; i < numFiles; ++i)
+	{
+	const char* name = zip_get_name(pArchive, i, 0);
+
+	zip_file* file = zip_fopen(pArchive, name, 0);
+	// Copy into buffer
+	zip_fread(file, data, maxsize);
+		//write to desy
+	}
+
+if (m_pArchive)
+    {
+    zip_close(m_pArchive);
+    m_pArchive = NULL;
+    }
+}
+void install()
+{
+AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);
+AAsset* asset = AAssetManager_open(mgr, (const char *) js, AASSET_MODE_UNKNOWN);
+if (NULL == asset) {
+//	__android_log_print(ANDROID_LOG_ERROR, NF_LOG_TAG, "_ASSET_NOT_FOUND_");
+	return JNI_FALSE;
+	}
+long size = AAsset_getLength(asset);
+char* buffer = (char*) malloc (sizeof(char)*size);
+AAsset_read (asset,buffer,size);
+//__android_log_print(ANDROID_LOG_ERROR, NF_LOG_TAG, buffer);
+AAsset_close(asset);
+}
+*/
+
+void assetM(void)
+{
+const char *namea;
+char buff[1025];
+struct AAssetManager *aaM=engine.app->activity->assetManager;
+struct AAssetDir *aaD;
+struct AAsset *aaS;
+aaD=AAssetManager_openDir(aaM,"");
+namea=AAssetDir_getNextFileName(aaD);
+while  (namea!=0) {
+	LOGI(namea);
+	aaS=AAssetManager_open(aaM, namea,AASSET_MODE_BUFFER);
+	long size = AAsset_getLength(aaS);
+	while (size>0) {
+		size=AAsset_read(aaS,buff,1024);buff[size]=0;
+		LOGI(buff);
+		size=AAsset_getRemainingLength(aaS);
+		}
+
+	AAsset_close(aaS);
+	namea=AAssetDir_getNextFileName(aaD);
+	}
+AAssetDir_close(aaD);
+}
+
 //----------------------------------------------------------------------------
 void android_main(struct android_app* state)
 {
@@ -1036,6 +1181,8 @@ while ((ident=ALooper_pollAll(engine.animating?0:-1,NULL,&events,(void**)&source
             return;
         }
     }
+
+assetM();
 
 strcpy(linea,rootpath);
 strcat(linea,"/main.txt");
