92468b73501199be24e1b3406765670de850de01
pabloreda <pabloreda@e1b2fb66-2cfc-11de-a28d-194e1977add9>
2013-03-25 18:47:35 +0000

    add multitouch in android VM (experimental)

--- trunk/MV/r4a/jni/Android.mk
+++ trunk/MV/r4a/jni/Android.mk
@@ -23,8 +23,8 @@ LOCAL_LDLIBS    := -lm -landroid
 LOCAL_STATIC_LIBRARIES := android_native_app_glue 
 LOCAL_ARM_MODE := arm 
 #LOCAL_CFLAGS += -O2 -march=armv7-a -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
-#LOCAL_CFLAGS += -O2 -mfpu=neon -mfloat-abi=softfp
-LOCAL_CFLAGS += -O8
+LOCAL_CFLAGS += -O2 -mfpu=neon -mfloat-abi=softfp
+#LOCAL_CFLAGS += -O8
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,android/native_app_glue)
