215298c8aae50d143a310b96ea95beb900327a09
Pablo Hugo Reda <pabloreda@gmail.com>
2017-12-06 00:55:16 -0300

    new 3dvector example, more compiler, more convert

--- r4/Compiler/code.asm
+++ r4/Compiler/code.asm
@@ -1,1 +1,1 @@
-;---:r4 compiler code.asm; **abc-- ** L ** len:12 ** calls:1; ** vreg:30 R:1 W:1 >W (0:10) 11 R:1 W:1 >W (0:2) 22 R:1 W:2 >W (0:10) 33	 CTE R:1 CPY U (7:8) 0	; :fill	 0 1 2w15:	; SWAP	 0 1 2	; >A	 0 2 1	; (	 0 2_1: 	; 1?	 0 2or $0,$0
+;---:r4 compiler code.asm; **abc-- ** L ** len:12 ** calls:1; ** vreg:30 R:1 W:1 >W (0:10) 11 R:1 W:1 >W (0:2) 22 R:1 W:2 >W (0:10) 33	 CTE R:1 CPY U (7:8) 0	; :fill	 0 1 2w15:	; SWAP	 0 1 2	; >A	 0 2 1mov esi,$0
